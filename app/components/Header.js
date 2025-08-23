d2a20848666a87a4d8e715fc06ef8a87dc3f8741
Harkaran Singh <harkaran2004.jpr@gmail.com>
2025-08-23 03:04:54 -0600

    gave some space

--- app/components/Header.js
+++ app/components/Header.js
@@ -13,6 +13,11 @@ export default function Header() {
   const [searchQuery, setSearchQuery] = useState('');
   const [user, setUser] = useState(null);
 
+
+
+
+
+  
   useEffect(() => {
     supabase.auth.getSession().then(({ data }) => {
       if (data.session) setUser(data.session.user);
